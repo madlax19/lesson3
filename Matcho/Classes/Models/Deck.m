2f745b202f71e354eec97edf707fcd91424b7157
Anton Lookin <anton.lookin@gmail.com>
2015-10-20 19:55:44 +0300

    Homework lesson 3

---
+++ Matcho/Classes/Models/Deck.m
@@ -0,0 +1,46 @@
+//
+//  Deck.m
+//  Matchismo
+//
+//  Created by Anton Lookin on 10/19/15.
+//  Copyright © 2015 geekub. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface Deck()
+
+@property (nonatomic, strong) NSMutableArray *cards;
+
+@end
+
+
+@implementation Deck
+
+- (NSMutableArray *)cards {
+	if (!_cards) {
+		_cards = [[NSMutableArray alloc] init];
+	}
+	return _cards;
+}
+
+
+- (void)addCard:(Card *)card {
+	[self addCard:card atTop:NO];
+}
+
+
+- (void)addCard:(Card *)card atTop:(BOOL)atTop {
+	if (atTop) {
+		[self.cards insertObject:card atIndex:0];
+	} else {
+		[self.cards addObject:card];
+	}
+}
+
+- (Card *)drawRandomCard {
+	NSInteger randomCard = arc4random() % [self.cards count];
+	return self.cards[randomCard];
+}
+
+@end
---
+++ Matcho/ViewController.m
@@ -0,0 +1,46 @@
+//
+//  ViewController.m
+//  Matcho
+//
+//  Created by Anton Lookin on 10/19/15.
+//  Copyright © 2015 geekub. All rights reserved.
+//
+
+#import "PlayingCardDeck.h"
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, strong) PlayingCardDeck *deck;
+
+@property (weak, nonatomic) IBOutlet UIButton *cardButton;
+
+@end
+
+@implementation ViewController
+
+- (PlayingCardDeck *)deck {
+	if (!_deck) {
+		_deck = [[PlayingCardDeck alloc] init];
+	}
+	return _deck;
+}
+
+
+- (IBAction)cardButtonTapped:(UIButton *)sender {
+	if ([sender.currentTitle length]) {
+		[sender setTitle:@""
+				forState:UIControlStateNormal];
+		[sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
+						  forState:UIControlStateNormal];
+	} else {
+		Card *card = [self.deck drawRandomCard];
+		[sender setTitle:card.contents
+				forState:UIControlStateNormal];
+		[sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+						  forState:UIControlStateNormal];
+	}
+}
+
+@end
